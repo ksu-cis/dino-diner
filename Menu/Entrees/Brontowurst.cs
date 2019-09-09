b9ea8ed7906be6a44d8151a79ffce02b3e6e36ce
Kelby Dinkel <kelbyd@users.campus>
2019-09-09 17:29:35 -0500

    Added Brauntoworst and Entree base class

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.AbstractClasses;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class Brontowurst : Entree
+    {
+        public Brontowurst()
+        {
+            ingredients = new List<string>() { "Brautwurst", "Whole Wheat Bun", "Peppers", "Onion" };
+            price = 5.36;
+            calories = 498;
+            count = 1;
+        }
+        
+        public void HoldBun()
+        {
+            ingredients.Remove( "Whole Wheat Bun" );
+        }
+        public void HoldPeppers()
+        {
+            ingredients.Remove( "Peppers" );
+        }
+        public void HoldOnion()
+        {
+            ingredients.Remove( "Onion" );
+        }
+    }
+}
