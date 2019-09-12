7c986b73618cb132d85f8708782fbf85ee1eb544
Cesar Zavala <cesarz@ksu.edu>
2019-09-11 20:16:06 -0500

    almost done

---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    /// <summary>
+    /// Manages the King burger.
+    /// </summary>
+    public class TRexKingBurger
+    {
+        private bool _ketchup = true;
+        private bool _lettuce = true;
+        private bool _mustard = true;
+        private bool _bun = true;
+        private bool _tomato = true;
+        private bool _pickle = true;
+        private bool _onion = true;
+        private bool _mayo = true;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> Ingredients = new List<string>() { "Steakburger Pattie", "Steakburger Pattie", "Steakburger Pattie" };
+                if (_lettuce) Ingredients.Add("Lettuce");
+                if (_mustard) Ingredients.Add("Mustard");
+                if (_ketchup) Ingredients.Add("Ketchup"); 
+                if (_bun) Ingredients.Add("Whole Wheat Bun");
+                if (_tomato) Ingredients.Add("Tomato");
+                if (_pickle) Ingredients.Add("Pickle");
+                if (_onion) Ingredients.Add("Onion");
+                if (_mayo) Ingredients.Add("Mayo");
+                return Ingredients;
+            }
+        }
+
+        /// <summary>
+        /// No parameter constructor.
+        /// </summary>
+        public TRexKingBurger()
+        {
+            this.Price = 8.45;
+            this.Calories = 728;
+        }
+        /// <summary>
+        /// Holds the ketchup.
+        /// </summary>
+        public void HoldKetchup()
+        {
+            _ketchup = false;
+        }
+        /// <summary>
+        /// Holds the lettuce.
+        /// </summary>
+        public void HoldLettuce()
+        {
+            _lettuce = false;
+        }
+        /// <summary>
+        /// Holds the mayo.
+        /// </summary>
+        public void HoldMayo()
+        {
+            _mayo = false;
+        }
+        /// <summary>
+        /// Holds the mustard.
+        /// </summary>
+        public void HoldMustard()
+        {
+            _mustard = false;
+        }
+        /// <summary>
+        /// Holds the bun.
+        /// </summary>
+        public void HoldBun()
+        {
+            _bun = false;
+        }
+        /// <summary>
+        /// Holds the onion.
+        /// </summary>
+        public void HoldOnion()
+        {
+            _onion = false;
+        }
+        /// <summary>
+        /// Holds the pickle.
+        /// </summary>
+        public void HoldPickle()
+        {
+            _pickle = false;
+        }
+        /// <summary>
+        /// Holds the tomato.
+        /// </summary>
+        public void HoldTomato()
+        {
+            _tomato = false;
+        }
+    }
+}
---
+++ Menu/Entrees/VelociWrap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    /// <summary>
+    /// Manages the VelociWrap.
+    /// </summary>
+    public class VelociWrap
+    {
+        private bool _dressing = true;
+        private bool _lettuce = true;
+        private bool _cheese = true;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> Ingredients = new List<string>() { "Flour Tortilla", "Chicken Breast" };
+                if (_dressing) Ingredients.Add("Ceasar Dressing");
+                if (_lettuce) Ingredients.Add("Romaine Lettuce");
+                if (_cheese) Ingredients.Add("Parmesan Cheese");
+                return Ingredients;
+            }
+        }
+
+        /// <summary>
+        /// No parameter constructor.
+        /// </summary>
+        public VelociWrap()
+        {
+            this.Price = 6.86;
+            this.Calories = 356;
+        }
+
+        /// <summary>
+        /// Dressing holding method.
+        /// </summary>
+        public void HoldDressing()
+        {
+            _dressing = false;
+        }
+
+        /// <summary>
+        /// Lettuce holding method.
+        /// </summary>
+        public void HoldLettuce()
+        {
+            _lettuce = false;
+        }
+
+        /// <summary>
+        /// Cheese holding method.
+        /// </summary>
+        public void HoldCheese()
+        {
+            _cheese = false;
+        }
+    }
+}
