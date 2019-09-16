c244d8eda5e4f858695a4231fc5d5651133afc14
cbcerna <cbcerna@CIS-RDS1>
2019-09-16 14:56:07 -0500

    Added the names of the new sides as classes

---
+++ Menu/Sides/Triceritots.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu.Sides
+{
+    class Triceritots
+    {
+        public Triceritots()
+        {
+            
+        }
+    }
+}
