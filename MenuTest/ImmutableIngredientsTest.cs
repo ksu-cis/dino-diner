4493590877a0dab5e32321c9274607699a301c11
Nathan Bean <nhbean@ksu.edu>
2019-10-02 17:13:10 -0500

    Milestone 4 content

---
+++ MenuTest/ImmutableIngredientsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest
+{
+    public class ImmutableIngredientsTest
+    {
+        [Theory]
+        [InlineData(typeof(Brontowurst))]
+        [InlineData(typeof(DinoNuggets))]
+        [InlineData(typeof(PrehistoricPBJ))]
+        [InlineData(typeof(PterodactylWings))]
+        [InlineData(typeof(SteakosaurusBurger))]
+        [InlineData(typeof(TRexKingBurger))]
+        [InlineData(typeof(VelociWrap))]
+        [InlineData(typeof(Fryceritops))]
+        [InlineData(typeof(Triceritots))]
+        [InlineData(typeof(MeteorMacAndCheese))]
+        [InlineData(typeof(MezzorellaSticks))]
+        [InlineData(typeof(Tyrannotea))]
+        [InlineData(typeof(Sodasaurus))]
+        [InlineData(typeof(JurassicJava))]
+        [InlineData(typeof(Water))]
+        public void IngredientsShouldBeImmutable(Type type)
+        {
+            IMenuItem item = (IMenuItem)Activator.CreateInstance(type);
+            item.Ingredients.Clear();
+            Assert.NotEmpty(item.Ingredients);
+        }
+    }
+}
---
+++ MenuTest/MenuItemNamesTest.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest
+{
+    public class MenuItemNamesTest
+    {
+        #region Entrees
+
+        [Fact]
+        public void BrontowurstToStringShouldGiveName()
+        {
+            Brontowurst bw = new Brontowurst();
+            Assert.Equal("Brontowurst", bw.ToString());
+        }
+
+
+        [Fact]
+        public void DinoNuggetToStringShouldGiveName()
+        {
+
+            DinoNuggets dn = new DinoNuggets();
+            Assert.Equal("Dino-Nuggets", dn.ToString());
+        }
+
+
+        [Fact]
+        public void PrehistoricPBJToStringShouldGiveName()
+        {
+            PrehistoricPBJ pbj = new PrehistoricPBJ();
+            Assert.Equal("Prehistoric PB&J", pbj.ToString());
+        }
+
+        [Fact]
+        public void PterodactylWingsToStringShouldGiveName()
+        {
+            PterodactylWings pw = new PterodactylWings();
+            Assert.Equal("Pterodactyl Wings", pw.ToString());
+        }
+
+        [Fact]
+        public void SteakosaurusBurgerToStringShouldGiveName()
+        {
+            SteakosaurusBurger sb = new SteakosaurusBurger();
+            Assert.Equal("Steakosaurus Burger", sb.ToString());
+        }
+
+        [Fact]
+        public void TRexKingBurgerToStringShouldGiveName()
+        {
+            TRexKingBurger trex = new TRexKingBurger();
+            Assert.Equal("T-Rex King Burger", trex.ToString());
+        }
+
+        [Fact]
+        public void VelociWrapToStringShouldGiveName()
+        {
+            VelociWrap vw = new VelociWrap();
+            Assert.Equal("Veloci-Wrap", vw.ToString());
+        }
+
+        #endregion
+
+        #region Sides
+
+        [Theory]
+        [InlineData(Size.Small)]
+        [InlineData(Size.Medium)]
+        [InlineData(Size.Large)]
+        public void FriceritopsToStringShouldGiveNameForSize(Size size)
+        {
+            Fryceritops ft = new Fryceritops();
+            ft.Size = size;
+            Assert.Equal($"{size} Friceritops", ft.ToString());
+        }
+
+        [Theory]
+        [InlineData(Size.Small)]
+        [InlineData(Size.Medium)]
+        [InlineData(Size.Large)]
+        public void MeteorMacAndCheeseToStringShouldGiveNameForSize(Size size)
+        {
+            MeteorMacAndCheese mmc = new MeteorMacAndCheese();
+            mmc.Size = size;
+            Assert.Equal($"{size} Meteor Mac and Cheese", mmc.ToString());
+        }
+
+        [Theory]
+        [InlineData(Size.Small)]
+        [InlineData(Size.Medium)]
+        [InlineData(Size.Large)]
+        public void MezzorellaSticksToStringShouldGiveNameForSize(Size size)
+        {
+            MezzorellaSticks ms = new MezzorellaSticks();
+            ms.Size = size;
+            Assert.Equal($"{size} Mezzorella Sticks", ms.ToString());
+        }
+
+        [Theory]
+        [InlineData(Size.Small)]
+        [InlineData(Size.Medium)]
+        [InlineData(Size.Large)]
+        public void ToStringShouldGiveNameForSize(Size size)
+        {
+            Triceritots tt = new Triceritots();
+            tt.Size = size;
+            Assert.Equal($"{size} Triceritots", tt.ToString());
+        }
+
+        #endregion
+
+        #region Drinks
+
+        [Theory]
+        [InlineData(Size.Small, false)]
+        [InlineData(Size.Medium, false)]
+        [InlineData(Size.Large, false)]
+        [InlineData(Size.Small, true)]
+        [InlineData(Size.Medium, true)]
+        [InlineData(Size.Large, true)]
+        public void JurrasicJavaToStringShouldGiveNameForSizeAndDecaf(Size size, bool decaf)
+        {
+            JurassicJava java = new JurassicJava();
+            java.Size = size;
+            java.Decaf = decaf;
+            if (decaf) Assert.Equal($"{size} Decaf Jurassic Java", java.ToString());
+            else Assert.Equal($"{size} Jurassic Java", java.ToString());
+        }
+
+
+        [Theory]
+        [InlineData(Size.Small, SodasaurusFlavor.Cherry)]
+        [InlineData(Size.Small, SodasaurusFlavor.Chocolate)]
+        [InlineData(Size.Small, SodasaurusFlavor.Cola)]
+        [InlineData(Size.Small, SodasaurusFlavor.Lime)]
+        [InlineData(Size.Small, SodasaurusFlavor.Orange)]
+        [InlineData(Size.Small, SodasaurusFlavor.RootBeer)]
+        [InlineData(Size.Small, SodasaurusFlavor.Vanilla)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Cherry)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Chocolate)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Cola)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Lime)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Orange)]
+        [InlineData(Size.Medium, SodasaurusFlavor.RootBeer)]
+        [InlineData(Size.Medium, SodasaurusFlavor.Vanilla)]
+        [InlineData(Size.Large, SodasaurusFlavor.Cherry)]
+        [InlineData(Size.Large, SodasaurusFlavor.Chocolate)]
+        [InlineData(Size.Large, SodasaurusFlavor.Cola)]
+        [InlineData(Size.Large, SodasaurusFlavor.Lime)]
+        [InlineData(Size.Large, SodasaurusFlavor.Orange)]
+        [InlineData(Size.Large, SodasaurusFlavor.RootBeer)]
+        [InlineData(Size.Large, SodasaurusFlavor.Vanilla)]
+        public void SodaSaurusToStringShouldGiveNameForSizeAndFlavor(Size size, SodasaurusFlavor flavor)
+        {
+            Sodasaurus soda = new Sodasaurus();
+            soda.Size = size;
+            soda.Flavor = flavor;
+            Assert.Equal($"{size} {flavor} Sodasaurus", soda.ToString());
+        }
+
+        [Theory]
+        [InlineData(Size.Small, false)]
+        [InlineData(Size.Medium, false)]
+        [InlineData(Size.Large, false)]
+        [InlineData(Size.Small, true)]
+        [InlineData(Size.Medium, true)]
+        [InlineData(Size.Large, true)]
+        public void TyrannoTeaToStringShouldGiveNameForSizeAndSweetness(Size size, bool sweet)
+        {
+            Tyrannotea tea = new Tyrannotea();
+            tea.Size = size;
+            tea.Sweet = sweet;
+            if (sweet) Assert.Equal($"{size} Sweet Tyrannotea", tea.ToString());
+            else Assert.Equal($"{size} Tyrannotea", tea.ToString());
+        }
+
+        [Theory]
+        [InlineData(Size.Small)]
+        [InlineData(Size.Medium)]
+        [InlineData(Size.Large)]
+        public void WaterToStringShouldGiveNameForSize(Size size)
+        {
+            Water water = new Water();
+            water.Size = size;
+            Assert.Equal($"{size} Water", water.ToString());
+        }
+
+        #endregion
+
+        #region Combos
+
+        [Theory]
+        [InlineData(typeof(Brontowurst), "Brontowurst Combo")]
+        [InlineData(typeof(DinoNuggets), "Dino-Nuggets Combo")]
+        [InlineData(typeof(PrehistoricPBJ), "Prehistoric PB&J Combo")]
+        [InlineData(typeof(PterodactylWings), "Pterodactyl Wings Combo")]
+        [InlineData(typeof(SteakosaurusBurger), "Steakosaurus Burger Combo")]
+        [InlineData(typeof(TRexKingBurger), "T-Rex King Burger Combo")]
+        [InlineData(typeof(VelociWrap), "Veloci-Wrap Combo")]
+        public void ToStringShouldGiveName(Type type, string name)
+        {
+            Entree entree = (Entree)Activator.CreateInstance(type);
+            CretaceousCombo combo = new CretaceousCombo(entree);
+            Assert.Equal(name, combo.ToString());
+        }
+
+        #endregion
+    }
+}
